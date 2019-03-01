f77515ea5c629ed7b5f2e22f2936901b7e052b26
njuhi <morphynjuh@mail.com>
2019-03-01 14:04:47 +0300

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Contact(firstside,secondside,thirdside,fourthside,fithside,sixthside){
+    firdside:first;
+    secondside:second;
+    thirdside:third;
+    fourthside:fourth;
+    fithside:fith;
+    sixthside:sixthside;
+}
+
+$(document).ready(function() {
+    $("#HOLD").submit(function(event) {
+      event.preventDefault();
