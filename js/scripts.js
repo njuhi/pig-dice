670989c4dc76e5fa05d222e2b849ad52f1d94af8
njuhi <morphynjuh@mail.com>
2019-03-04 15:12:34 +0300

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,1 @@
-function Contact(firstside,secondside,thirdside,fourthside,fithside,sixthside){
-    firdside:first;
-    secondside:second;
-    thirdside:third;
-    fourthside:fourth;
-    fithside:fith;
-    sixthside:sixthside;
-}
 
-$(document).ready(function() {
-    $("#HOLD").submit(function(event) {
-      event.preventDefault();
