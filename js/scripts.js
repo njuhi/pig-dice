ea4c41142e5785bf9f502fc9f74c8a55acbb066c
njuhi <morphynjuh@mail.com>
2019-03-05 11:26:59 +0300

    save changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
+var dice = {
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+
+
+
+function printNumber(number) {
+  var player1 = document.getElementById('player1');
+  player1.innerHTML = number;
+}
+
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
+
 
